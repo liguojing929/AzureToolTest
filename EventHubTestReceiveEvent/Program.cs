d2f001af7c447efbd7d9fdfb98604fa8cbdbaf57
Daniel <dannyli0818@protonmail.com>
2023-04-07 16:45:26 +0800

    to receive events

--- EventHubTestReceiveEvent/Program.cs
+++ EventHubTestReceiveEvent/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Processor;
+using Azure.Storage.Blobs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +9,50 @@ using System.Threading.Tasks;
 
 namespace EventHubTestReceiveEvent
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
+            Console.WriteLine("Start Event Hub Receiver...");
+
+            string nameSpaceConnectionString = "Endpoint=sb://eventhubtestpersonal.servicebus.windows.net/;SharedAccessKeyName=sendandreceive;SharedAccessKey=U5un8lvQTaKo2Xd8U/4IMVAhXhINmppVv+AEhPQhIt4=;EntityPath=testeventhub";
+            string eventHubName = "testeventhub";
+
+            string blobConnectionString = "DefaultEndpointsProtocol=https;AccountName=eventhubteststorage2;AccountKey=cacwGETegYX6t/I73k84o381ABPsqSGBYogHbsPyA9iAOxJomm8HckY9ysLOIR3WvdfXUrsWAL3++AStMPc7Ng==;EndpointSuffix=core.windows.net";
+            string containerName = "offsetcontainer";
+
+            /** 
+             * the blob container client is used to store the check point. (check point can be used to record where we consumed the event last time and 
+             * use the UpdateCheckPointAsync method to avoid repeated consumption.
+             * */
+            BlobContainerClient blobContainerClient = new BlobContainerClient(blobConnectionString, containerName);
+            EventProcessorClient processor = new EventProcessorClient(blobContainerClient, "$Default", nameSpaceConnectionString, eventHubName);
+
+            processor.ProcessEventAsync += Processor_ProcessEventAsync;
+            processor.ProcessErrorAsync += Processor_ProcessErrorAsync;
+
+            await processor.StartProcessingAsync();
+            Console.WriteLine("Start the processor");
+
+            Console.ReadLine();
+            await processor.StopProcessingAsync();
+            Console.WriteLine("Stop the processor");
+            
+        }
+
+        private static Task Processor_ProcessErrorAsync(ProcessErrorEventArgs arg)
+        {
+            Console.WriteLine("Error Received: " + arg.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
+        private static async Task Processor_ProcessEventAsync(ProcessEventArgs arg)
+        {
+            Console.WriteLine($"Event Received from Partition {arg.Partition.PartitionId}: {arg.Data.EventBody.ToString()}");
+
+            // this is to tell EvenHub to get the offset from blob container and read through eventhub where we consume last time
+            // and make sure there won't be the repeated consumption.
+            await arg.UpdateCheckpointAsync();
         }
     }
 }
